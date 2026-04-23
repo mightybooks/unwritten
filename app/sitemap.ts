4c2e361c7d89a9f802b799c26a66b0366397c161
Mun Surim <surimstudio@gmail.com>
2026-04-23 14:27:30 +0900

    plus7

---
+++ app/sitemap.ts
@@ -0,0 +1,23 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://unwritten-psi.vercel.app',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://unwritten-psi.vercel.app/result?type=meursault',
+    },
+    {
+      url: 'https://unwritten-psi.vercel.app/result?type=bartleby',
+    },
+    {
+      url: 'https://unwritten-psi.vercel.app/result?type=valjean',
+    },
+    {
+      url: 'https://unwritten-psi.vercel.app/result?type=donquixote',
+    },
+    {
+      url: 'https://unwritten-psi.vercel.app/result?type=hamlet',
+    },
+  ];
+}
